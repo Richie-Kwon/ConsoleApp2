44dac02f5e2bcef3a72428c5b41d2a0a393e7b30
richie <richie.kwon1@gmail.com>
2021-10-17 17:52:32 +0100

    day3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApp2
 {
@@ -6,10 +7,28 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            int number = 1_234;
+            const int A = 4;
+            // Nullable
+            int? number2 = null;
+            var result = "odd";
+            string multilines = @"
+            Hello
+            world";
+            Console.WriteLine($"{A} is {result}");
             
+            // string.Format()
+            var msg = string.Format("{0},{1}", "Yera", "wins");
+            
+            // Boolean
+            var bln = true;
+            Int32 number100 = 5;
+            
+            Console.WriteLine("${DateTime.Now.Date}!");
+            Console.WriteLine("{0}", "Bye");
+            Console.WriteLine("{0}", "Bye");
         }
-         
-    }  
-}  
+    }
+}
