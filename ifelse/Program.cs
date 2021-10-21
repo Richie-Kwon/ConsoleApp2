6991f47407febed3c3e33648bc74f199a333c8f8
richie <richie.kwon1@gmail.com>
2021-10-21 17:28:22 +0100

    condition

---
+++ ifelse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            const int diff = 0;
+            const int first = 5;
+            const int second = 3;
+
+            string[] names = {"C", "C++"};
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            int sum = 0;
+            for (int i=1; i <=100; i++ )
+            {
+                if (i % 3 == 0)
+                {
+                    continue;
+                }
+                sum += i ;
+            }
+
+
+
+        }
+    }
+}
