65e23ff3084fc2c5ed1325e58e016c7d906c36c4
richie <richie.kwon1@gmail.com>
2021-10-22 18:16:00 +0100

    function1

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Array
 {
@@ -7,6 +8,34 @@ namespace Array
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            int [] perfectNumbers = {6,28,496} ;
+            Console.WriteLine(perfectNumbers[0]);
+
+            int[] eng = new int [3];
+            eng[0] = 0;
+            eng[1] = 1;
+            eng[2] = 2;
+            for (int i = 0; i < eng.Length; i++)
+            {
+                Console.Write(eng[i]);
+            }     
+            
+            int[] eng2 = new int [3]{4,5,6};
+            for (int j = 0; j < eng.Length; j++)
+            {
+                Console.Write(eng[j]);
+            }
+
+            int[] mvp = {7, 8, 9};
+            int[,] mvp2 = new int [2, 3];
+            int[,] mvp3 = {{1,2,3},{4,5,6}};
+            
+            // list
+            // https://www.tutorialsteacher.com/csharp/csharp-list
+            
+
+
         }
-    }
+    } 
 }
---
+++ Function/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int n = Convert.ToInt32(args[0]);
+            Factorial(n);
+        }
+        
+        // recursion
+        static int Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+
+        static void Sum() => Console.WriteLine("Sum: ");
+        
+
+    }
+}
