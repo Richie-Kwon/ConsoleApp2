ac89995838066711a84cc181e8b11eb42adf0124
richie <richie.kwon1@gmail.com>
2021-10-24 16:15:59 +0100

    enum

---
+++ Enum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+// 하나의 이름으로 서로 관련있는 정수값을 가지는 상수 집합을 정의
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Animal a = Animal.cats;
+            Console.WriteLine(a);
+
+            Animal b = Animal.dogs;
+            Console.WriteLine(b);
+            
+            Console.WriteLine(nameof(Animal.Mouse));
+        }
+
+        enum Animal
+        {
+            Mouse,
+            Tiger,
+            cats,
+            dogs
+        }
+    }
+}
--- StructureType/Program.cs
+++ StructureType/Program.cs
@@ -2,11 +2,26 @@
 
 namespace StructureType
 {
+    public struct Employee
+    {
+        public static string Name;
+        public static int Age;
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Employee.Age = 5;
+            Employee.Name = "Richie";
+            TimeSpan ts = (DateTime.Now - (new DateTime(2020,1,1))
+                );
+            Console.WriteLine(ts.TotalDays);
+
         }
     }
+    
+   
+
 }
