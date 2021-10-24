e8c2a08547689fa0e4c85f0cfc359b529b6cf488
richie <richie.kwon1@gmail.com>
2021-10-24 15:28:11 +0100

    structure type

---
+++ Function2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Function2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Hi();
+        }
+
+        static void Hi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}  
