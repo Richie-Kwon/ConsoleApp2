150b4b8130da1979ebf0fce4865ff15f80ef3eb4
richie <richie.kwon1@gmail.com>
2021-10-25 17:26:37 +0100

    class

---
+++ Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Square square1 = new Square();
+            square1.Weight = 10;
+            Console.WriteLine(square1.Weight);
+        }
+    }
+
+    class Square
+    {
+        public int Width;
+        public int Weight;
+        public static string Creator;
+        
+    }
+
+}   
