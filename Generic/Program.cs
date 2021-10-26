b59e6c2db7351f4af14a5c8433973510a7653640
richie <richie.kwon1@gmail.com>
2021-10-26 18:37:54 +0100

    LINQ

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Generic
 {
@@ -15,7 +16,26 @@ namespace Generic
 
             List<int> list = new List<int>();
             list.Add(100);
+            list.Insert(1, 200);
             Console.WriteLine(list[0]);
+
+            for (int i =0; i< list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+                
+            }
+
+            List<int> list2 = new List<int>();
+            list2.AddRange(Enumerable.Range(1, 10));
+
+            Dictionary<int, string> todo = new Dictionary<int, string>();
+            todo.Add(1, "Miss");
+            todo.Add(2, "New");
+
+            foreach (var item in todo)
+            { 
+                Console.WriteLine($"{item.Key} and {item.Value}");
+            }
 
         }  
     }
---
+++ Null/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace Null
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string s = null;
+            s = "Hello";
+            
+
+            string empty = "";
+
+            Nullable<bool> bln = null;
+            Console.WriteLine(bln.HasValue);
+
+            int? i = null;
+
+            string s2 = null;
+
+            empty = s2 ?? "change the value"; //if it is null >> change the value"
+            Console.WriteLine(empty);
+
+            double? d = 1.0;
+            Console.WriteLine(d?.ToString());
+
+            List<string> list22 = null;
+            int? aa = list22?.Count;
+            Console.WriteLine(aa);
+
+            // int bb = list22?.Count ?? 0; 
+
+
+
+
+
+
+
+        }
+    }
+}
