d38095e6adf88c5f599d441822aed98b8b2b60cf
richie <richie.kwon1@gmail.com>
2021-10-27 17:26:32 +0100

    oop starts

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace LINQ
 {
@@ -7,6 +10,30 @@ namespace LINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[] numbers = { 1, 2, 4 };
+            Console.WriteLine(numbers.Sum());
+
+            List<int> a = new List<int>();
+            
+            a.AddRange(numbers.Where(n => n % 2 == 0));
+                // numbers.Where(n => n % 2 == 0).ToList();
+            
+            for (int i =0; i< a.Count; i++)
+            {
+                Console.WriteLine(a[i]);
+            
+            }
+            
+            // Query syntax
+            // var query = from n in a
+            //     where n % 2 == 0;
+            // Select n;
+
+
+
+
+
         }
     }
 }
---
+++ OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] data1 = { 1, 2, 5, 3, 4 };
+            Array.Sort(data1);
+            data1 = data1.OrderByDescending(x => x).ToArray();
+
+            foreach (var i in data1)
+            {
+                Console.WriteLine(i);
+            }
+            
+            // int[] data2 = Array.Sort(data);
+              
+            
+        }
+    }
+}
