a2090b2153aa3f5635825eacfab8e43741ba168e
richie <richie.kwon1@gmail.com>
2021-10-28 17:49:59 +0100

    field

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Linq;
 
 namespace OOP
@@ -10,17 +11,35 @@ namespace OOP
             Console.WriteLine("Hello World!");
 
             int[] data1 = { 1, 2, 5, 3, 4 };
-            Array.Sort(data1);
-            data1 = data1.OrderByDescending(x => x).ToArray();
 
-            foreach (var i in data1)
-            {
-                Console.WriteLine(i);
-            }
-            
-            // int[] data2 = Array.Sort(data);
+            // int [] data2 = data1.OrderBy(x => x).ToArray();
+            // // Array.Sort(data1);
+            // // data1 = data1.OrderByDescending(x => x).ToArray();
+            //
+            // foreach (var i in data2)
+            // {
+            //     Console.WriteLine(i);
+            // }
               
+            // int[] data2 = Array.Sort(data);
             
+            // selection sort
+
+            for (int i=0; i < data1.Length-1; i++) 
+            {
+                for (int j = i+1; j< data1.Length; j++) 
+                {
+                    if (data1[i] > data1[j])
+                    {
+                        int temp = data1[i];
+                        data1[i] = data1[j];
+                        data1[j] = temp;
+                    }
+
+                }
+
+            }
+
         }
     }
 }
---
+++ OOP2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Code code1 = new Code();
+            code1.Go();
+        }
+    }
+
+    public class Code
+    {
+        public void Go() => Console.WriteLine("Start coding");
+        public override string ToString() => "My code";
+    }
+}
