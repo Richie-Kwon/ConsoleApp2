0aefa07ea60a7a4288d4143943fdfa47a4500e94
richie <richie.kwon1@gmail.com>
2022-01-04 21:35:04 +0000

    thread

--- Thread/Program.cs
+++ Thread/Program.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 
-namespace Function2
+namespace Thread
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Hi();
-        }
-
-        static void Hi()
-        {
-            Console.WriteLine("Hi");
         }
     }
-} 
+}
