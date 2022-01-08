1f2871f2dece212eb5fbbc01303ab47b9b741afb
richie <richie.kwon1@gmail.com>
2022-01-08 14:15:58 +0000

    in memory DB

---
+++ inmemory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace inmemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
